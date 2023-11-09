544f4ef404016bcecdc61bd42888d670f093a1b7
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2023-11-09 06:57:26 +0100

    initial commit

--- build.sbt
+++ build.sbt
@@ -1,16 +1,25 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.0"
+ThisBuild / scalaVersion := "2.12.17"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "udemykafkasparkcassandrarealtime"
+      name := "udemykafkasparkcassandrarealtime"
   )
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "3.5.0",
-  "org.apache.spark" %% "spark-sql" % "3.5.0",
-  "org.apache.spark" %% "spark-mllib" % "3.5.0",
+  "org.apache.spark" %% "spark-core" % "3.3.2",
+  "org.apache.spark" %% "spark-sql" % "3.3.2",
+  "org.apache.spark" %% "spark-mllib" % "3.3.2",
   "org.apache.logging.log4j" % "log4j-api" % "2.21.1",
-  "org.apache.logging.log4j" % "log4j-core" % "2.21.1"
+  "org.apache.logging.log4j" % "log4j-core" % "2.21.1",
+  // https://mvnrepository.com/artifact/org.apache.spark/spark-hive
+  "org.apache.spark" %% "spark-hive" % "3.3.2",
+
+  // https://mvnrepository.com/artifact/com.datastax.spark/spark-cassandra-connector
+  "com.datastax.spark" %% "spark-cassandra-connector" % "3.4.1",
+  "org.apache.spark" %% "spark-streaming" % "3.3.2",
+  // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10
+  "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.3.2",
+  "org.apache.spark" %% "spark-sql-kafka-0-10" % "3.3.2"
 )
--- src/main/scala/KafkaCassandraSpark.scala
+++ src/main/scala/KafkaCassandraSpark.scala
@@ -1,10 +1,61 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.types.{StringType, StructType}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.streaming.Trigger
 
+
+/*
+https://sparkbyexamples.com/spark/spark-streaming-with-kafka/
+Create a topic json_topic
+
+ */
 object KafkaCassandraSpark {
 
   def main(args: Array[String]): Unit = {
     val spark =
-      SparkSession.builder.master("local[*]").appName("kafka_cassandra").enableHiveSupport().getOrCreate()
-  }
+      SparkSession
+        .builder
+        .master("local[*]")
+        .appName("kafka_cassandra")
+        .enableHiveSupport()
+        .getOrCreate()
 
+    spark.conf.set("spark.sql.adaptive.enabled",false)
+    spark.conf.set("spark.sql.streaming.forceDeleteTempCheckpointLocation",true)
+
+    def writeToCassandra(writeDF: DataFrame, epochId:Long): Unit = {
+      writeDF
+        .write
+        .format("org.apache.spark.sql.cassandra")
+        .option("table","got_char_data")
+        .option("keyspace","got_db")
+        .mode("append")
+        .save()
+    }
+
+    val df = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers","127.0.0.1:9092")
+      .option("subscribe","json_topic")
+      .option("startingOffsets","earliest")
+      .load()
+
+    val personStringDF = df.selectExpr("CAST(value AS STRING)")
+
+    val schema = new StructType()
+      .add("charactername", StringType)
+      .add("actorname", StringType)
+      .add("housename", StringType)
+      .add("nickname", StringType)
+
+    val personDF = personStringDF.select(from_json(col("value"), schema).as("data"))
+      .select("data.*")
+
+    personDF.writeStream
+      .trigger(Trigger.ProcessingTime("5 seconds"))
+      .outputMode("append")
+      .foreachBatch(writeToCassandra _)
+      .start()
+      .awaitTermination()
+  }
 }
