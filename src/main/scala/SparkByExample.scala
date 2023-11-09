4785d54d75d124dad16a10354af0083f65229929
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2023-11-09 07:33:53 +0100

    initial commit

--- src/main/scala/SparkByExample.scala
+++ src/main/scala/SparkByExample.scala
@@ -1,15 +1,18 @@
 import org.apache.spark.sql.functions.{col, from_json}
-import org.apache.spark.sql.types.{StringType, StructType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.streaming.Trigger
 
 
 /*
 https://sparkbyexamples.com/spark/spark-streaming-with-kafka/
-Create a topic json_topic
+-Create a topic json_topic
+-Run kafka-topics --create --replication-factor 1 --partitions 1 --topic json_topic --bootstrap-server localhost:9092
+
+
 
  */
-object KafkaCassandraSpark {
+object SparkByExample {
 
   def main(args: Array[String]): Unit = {
     val spark =
@@ -23,15 +26,6 @@ object KafkaCassandraSpark {
     spark.conf.set("spark.sql.adaptive.enabled",false)
     spark.conf.set("spark.sql.streaming.forceDeleteTempCheckpointLocation",true)
 
-    def writeToCassandra(writeDF: DataFrame, epochId:Long): Unit = {
-      writeDF
-        .write
-        .format("org.apache.spark.sql.cassandra")
-        .option("table","got_char_data")
-        .option("keyspace","got_db")
-        .mode("append")
-        .save()
-    }
 
     val df = spark.readStream
       .format("kafka")
@@ -40,22 +34,33 @@ object KafkaCassandraSpark {
       .option("startingOffsets","earliest")
       .load()
 
+    df.printSchema()
+
+    // Spark Streaming Write to Console
+    // Since the value is in binary
+    // first we need to convert the binary value to String using selectExpr()
     val personStringDF = df.selectExpr("CAST(value AS STRING)")
 
+
     val schema = new StructType()
-      .add("charactername", StringType)
-      .add("actorname", StringType)
-      .add("housename", StringType)
-      .add("nickname", StringType)
+      .add("id", IntegerType)
+      .add("firstname", StringType)
+      .add("middlename", StringType)
+      .add("lastname", StringType)
+      .add("dob_year", IntegerType)
+      .add("dob_month", IntegerType)
+      .add("gender", StringType)
+      .add("salary", IntegerType)
 
     val personDF = personStringDF.select(from_json(col("value"), schema).as("data"))
       .select("data.*")
 
+
     personDF.writeStream
-      .trigger(Trigger.ProcessingTime("5 seconds"))
+      .format("console")
       .outputMode("append")
-      .foreachBatch(writeToCassandra _)
       .start()
       .awaitTermination()
+
   }
 }
